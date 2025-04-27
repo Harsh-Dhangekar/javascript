b943a1490c8b9344f6e36f7d9db059e395248e1f
Harsh-Dhangekar <harshdhangekar3@gmail.com>
2025-04-27 07:29:26 +0000

    variables of javascript

---
+++ 01_basics/demovariables.js
@@ -0,0 +1,12 @@
+let student_name = "Harsh"
+let student_id =12345
+let student_marks=99.99
+let Place="Ravet" 
+
+console.table([student_id,student_marks,Place,student_name]);
+
+ student_name="ABC"
+ student_id =102030
+ student_marks=100
+ Place="Pune" 
+console.table([student_id,student_marks,Place,student_name])
