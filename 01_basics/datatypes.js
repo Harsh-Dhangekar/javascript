9b2e63cf9e81d435a69746af566dedb3386d470c
Harsh-Dhangekar <harshdhangekar3@gmail.com>
2025-04-28 12:48:06 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict";// it treat all JS code as newer version of JS
+
+//alert("hello")//we are  using nodejs not browser.
+
+let name = "Harsh"// string 
+let age = 21// number 
+let Log_In= false//boolean True or false 
+let account_Id = null// null is consider as standalon value
+
+console.log(name);
+console.log(age);
+console.log(Log_In);
+console.log(account_Id);
+
+console.log(typeof null);// the null gives value as object 
+console.log(typeof undefined);// the undifined gives value as undefined  
